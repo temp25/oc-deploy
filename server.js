d637380c79211478c5240bfe22e98f0b9b1616f2
temp25 <39752837+temp25@users.noreply.github.com>
2020-09-18 15:50:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,51 +1,94 @@
-/*jslint node: true */
-/*jshint esversion: 6 */
-
-"use strict";
-
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 3000;
 const app = express();
-const fs = require("fs");
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static("static"));
+app.get('/executeCommand', function(req, res) {
 
-app.get("/executeCommand", (request, response) => {
-  console.log(new Date() + "    executeCommand invoked");
-  console.log("dumping custom request args....");
-  let command = request.query.command;
-  let args = request.query.args;
-  console.log(command);
-  console.log(args);
+  var clientIPAddress = getIPFromRequest(req);
 
-  const headers = {
-    "Content-Type": "text/event-stream",
-    Connection: "keep-alive",
-    "Cache-Control": "no-cache"
-  };
-  response.writeHead(200, headers);
-  
+  console.log(`Deployment initiated from client, ${clientIPAddress}`);
+
+  let command = '';
+  let args = '';
+
+  if(req.query.command !== undefined) {
+    command = req.query.command;
+  }
+
+  if(req.query.args !== undefined) {
+    args = req.query.args;
+  }
+
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive'
+  });
+
   const childProcess = require("child_process");
-  const customProcess = childProcess.spawn("/app/test.sh");
+  const customProcess = childProcess.spawn(path.join(__dirname, command), args.split(","));
 
   customProcess.stdout.on("data", data => {
-    console.log("STDOUT : " + data);
-    response.write("STDOUT : " + data);
+    sendData(res, `STDOUT : ${data}`);
   });
 
   customProcess.stderr.on("data", data => {
-    console.log("STDERR : " + data);
-    response.write("STDERR : " + data);
+    sendData(res, `STDERR : ${data}`);
   });
 
   customProcess.on("close", code => {
-    response.write(`child process exited with code ${code}`);
+    sendData(res, `child process exited with code ${code}`);
+    res.end();
   });
+
 });
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, () => {
-  console.log(`Your app is listening on port ${listener.address().port}`);
-});
+function sendData(res, data) {
+  console.log(data);
+  res.write(`data: ${data}\n\n`);
+}
+
+function getIPFromRequest(req) {
+
+  let xForwardedFor = req.headers['x-forwarded-for'];
+  let reqConnectionRemoteAddress;
+  let reqConnectionSocketRemoteAddress;
+  let reqSocketRemoteAddress;
+
+  if(xForwardedFor != undefined){
+    xForwardedFor = xForwardedFor.split(',').pop().trim();
+  }
+
+  if(req.connection !== undefined) {
+    reqConnectionRemoteAddress = req.connection.remoteAddress;
+    reqConnectionSocketRemoteAddress = req.connection.socket !== undefined ? req.connection.socket.remoteAddress : undefined; 
+  } else {
+    reqConnectionRemoteAddress = reqConnectionSocketRemoteAddress = undefined;
+  }
+
+  if(req.socket != undefined && req.socket.remoteAddress !== undefined) {
+    reqSocketRemoteAddress = req.socket.remoteAddress;
+  }
+
+  if(xForwardedFor !== undefined) {
+    console.debug("Using IP from xForwardedFor header");
+    return xForwardedFor;
+  } else if(reqConnectionRemoteAddress !== undefined) {
+    console.debug('Using IP from req.connection.remoteAddress');
+    return reqConnectionRemoteAddress;
+  } else if(reqSocketRemoteAddress !== undefined) {
+    console.debug('Using IP from req.socket.remoteAddress');
+    return reqSocketRemoteAddress;
+  } else if(reqConnectionSocketRemoteAddress !== undefined) {
+    console.debug('Using IP from req.connection.socket.remoteAddress');
+    return reqConnectionSocketRemoteAddress;
+  } else {
+    console.debug('Couldn\'t fetch IP address from request object');
+    return null;
+  }
+
+}
+
+app.listen(PORT, () => console.log(`SSE app listening on port ${PORT}!!!`));
